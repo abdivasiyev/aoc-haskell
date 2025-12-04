1a7f530705c84335cddb8ebac3551e44630ce759
Asliddin Abdivasiyev <asliddin.abdivasiyev@gmail.com>
2025-12-04 16:47:14 +0500

    some formattings

--- src/Aoc.hs
+++ src/Aoc.hs
@@ -1,34 +1,36 @@
-{-# Language ExistentialQuantification #-}
-{-# Language OverloadedStrings #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-module Aoc (
-     Year
-   , Day
-   , Solution(..)
-   , AnySolution(..)
-   , RunnerMode(..)
-   , Runner(..)
-   , run
-   , splitByPosition
-   , md5sum
-) where
+module Aoc
+  ( Year,
+    Day,
+    Solution (..),
+    AnySolution (..),
+    RunnerMode (..),
+    Runner (..),
+    run,
+    splitByPosition,
+    md5sum,
+  )
+where
 
-import           Text.Printf
-import           Data.List
-import           Control.Monad
-import qualified Data.ByteString.Char8 as BS
-import           Crypto.Hash (hashWith, MD5 (..))
+import Control.Monad
+import Crypto.Hash (MD5 (..), hashWith)
+import Data.ByteString.Char8 qualified as BS
+import Data.List
+import Text.Printf
 
 type Year = Int
-type Day  = Int
+
+type Day = Int
 
 class Solution a where
-    day   :: a      -> (Year, Day)
-    parse :: a      -> [String]    -> a
-    part1 :: a      -> [String]
-    part2 :: a      -> [String]
+  day :: a -> (Year, Day)
+  parse :: a -> [String] -> a
+  part1 :: a -> [String]
+  part2 :: a -> [String]
 
-data AnySolution = forall s. Solution s => AnySolution s
+data AnySolution = forall s. (Solution s) => AnySolution s
 
 data RunnerMode = All | Last | Single (Year, Day)
 
@@ -36,31 +38,31 @@ data Runner = Runner {mode :: RunnerMode, solutions :: [AnySolution]}
 
 solve :: AnySolution -> IO ()
 solve (AnySolution s) = do
-    let (y, d) = day s
-    let inputPath = intercalate "_" [printf "%04d" y, printf "%02d" d] <> ".txt"
-    input <- readFile inputPath
-    let s' = parse s $ lines input
-    let p1 = part1 s'
-    let p2 = part2 s'
+  let (y, d) = day s
+  let inputPath = intercalate "_" [printf "%04d" y, printf "%02d" d] <> ".txt"
+  input <- readFile inputPath
+  let s' = parse s $ lines input
+  let p1 = part1 s'
+  let p2 = part2 s'
 
-    putStrLn $ "Year " <> show y <> ", Day " <> show d
-    putStrLn $ "Part 1: " <> intercalate ", " p1
-    putStrLn $ "Part 2: " <> intercalate ", " p2
+  putStrLn $ "Year " <> show y <> ", Day " <> show d
+  putStrLn $ "Part 1: " <> intercalate ", " p1
+  putStrLn $ "Part 2: " <> intercalate ", " p2
 
 dayMatches :: Year -> Day -> AnySolution -> Bool
 dayMatches y d (AnySolution s) = day s == (y, d)
 
 run :: Runner -> IO ()
 run (Runner m ss) = case m of
-             All -> forM_ (reverse ss) solve
-             Last -> solve $ last ss
-             Single (y, d) -> case find (dayMatches y d) ss of
-                                   Nothing -> putStrLn "Solution not found"
-                                   Just s -> solve s
+  All -> forM_ (reverse ss) solve
+  Last -> solve $ last ss
+  Single (y, d) -> case find (dayMatches y d) ss of
+    Nothing -> putStrLn "Solution not found"
+    Just s -> solve s
 
 splitByPosition :: [a] -> ([a], [a])
 splitByPosition [] = ([], [])
-splitByPosition (x:xs) = let (odds, evens) = splitByPosition xs in (x:evens, odds)
+splitByPosition (x : xs) = let (odds, evens) = splitByPosition xs in (x : evens, odds)
 
 md5sum :: String -> String
 md5sum xs = show . hashWith MD5 . BS.pack $ xs
--- src/Aoc/Years/Year2015/Day2.hs
+++ src/Aoc/Years/Year2015/Day2.hs
@@ -1,42 +1,42 @@
-module Aoc.Years.Year2015.Day2 (
-    Day2(..)
-) where
+module Aoc.Years.Year2015.Day2
+  ( Day2 (..),
+  )
+where
 
 import Aoc
-import Data.List.Extra (splitOn)
 import Data.List (sort)
+import Data.List.Extra (splitOn)
 
 data Day2 = Day2 | Input [Prism]
 
 instance Solution Day2 where
-    day _ = (2015, 2)
+  day _ = (2015, 2)
 
-    parse Day2 input = Input . map prism . map (splitOn "x") $ input 
-    parse _ _         = undefined
+  parse Day2 input = Input . map prism . map (splitOn "x") $ input
+  parse _ _ = undefined
 
-    part1 (Input xs) = [show . sum . map (\x -> surfaceArea x + smallestArea x) $ xs]
-    part1 _          = undefined
+  part1 (Input xs) = [show . sum . map (\x -> surfaceArea x + smallestArea x) $ xs]
+  part1 _ = undefined
 
-    part2 (Input xs) = [show . sum . map (\x -> volume x + smallestPerimeter x) $ xs]
-    part2 _          = undefined
+  part2 (Input xs) = [show . sum . map (\x -> volume x + smallestPerimeter x) $ xs]
+  part2 _ = undefined
 
 data Prism = Prism Int Int Int
 
 prism :: [String] -> Prism
 prism [l, w, h] = Prism (read l) (read w) (read h)
-prism _         = undefined 
+prism _ = undefined
 
-volume              :: Prism -> Int
+volume :: Prism -> Int
 volume (Prism l w h) = l * w * h
 
-surfaceArea              :: Prism -> Int
-surfaceArea (Prism l w h) = 2 * (l*w + w*h + h*l)
+surfaceArea :: Prism -> Int
+surfaceArea (Prism l w h) = 2 * (l * w + w * h + h * l)
 
-smallestArea              :: Prism -> Int
-smallestArea (Prism l w h) = 
-    let [a, b, _] = sort [l, w, h] in a * b
+smallestArea :: Prism -> Int
+smallestArea (Prism l w h) =
+  let [a, b, _] = sort [l, w, h] in a * b
 
-smallestPerimeter              :: Prism -> Int
+smallestPerimeter :: Prism -> Int
 smallestPerimeter (Prism l w h) =
-    let [a, b, _] = sort [l, w, h] in 2 * (a+b)
-
+  let [a, b, _] = sort [l, w, h] in 2 * (a + b)
