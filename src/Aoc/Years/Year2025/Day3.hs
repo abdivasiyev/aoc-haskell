8d295c0cdfe6b14d8ff6bd7533ec44165899ca20
Asliddin Abdivasiyev <asliddin.abdivasiyev@gmail.com>
2025-12-05 11:56:45 +0500

    Year 2025, Day 3 Part 1

--- src/Aoc/Years/Year2025/Day3.hs
+++ src/Aoc/Years/Year2025/Day3.hs
@@ -4,15 +4,35 @@ module Aoc.Years.Year2025.Day3
 where
 
 import Aoc
+import Data.Char (digitToInt)
 
-data Day3 = Day3 | Input String
+newtype Rating = Rating Int deriving (Show)
+
+newtype Bank = Bank [[Rating]]
+
+data Day3 = Day3 | Input Bank
 
 instance Solution Day3 where
   day Day3 = (2025, 3)
   day _ = undefined
-  parse Day3 input = Input $ head input
+  parse Day3 input = Input . Bank $ map parseLine input
+    where
+      parseLine :: String -> [Rating]
+      parseLine [] = []
+      parseLine (x : xs) = Rating (digitToInt x) : parseLine xs
   parse _ _ = undefined
-  part1 (Input _) = ["unsolved"]
+  part1 (Input (Bank xs)) = [show $ sum . map go $ xs]
+    where
+      go :: [Rating] -> Int
+      go ratings =
+        maximum
+          [ z
+          | i <- [0 .. length ratings - 2],
+            let Rating x = ratings !! i,
+            j <- [i + 1 .. length ratings - 1],
+            let Rating y = ratings !! j,
+            let z = x * 10 + y
+          ]
   part1 _ = undefined
   part2 (Input _) = ["unsolved"]
   part2 _ = undefined
